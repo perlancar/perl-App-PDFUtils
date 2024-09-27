556bfbf03bcbb6b86468e817661c0ca1d7d11f5f
perlancar (on notebook-dell-xps13-9360-dec2017) <perlancar@gmail.com>
2024-09-28 05:41:31 +0700

    Split convert-epub-to-pdf to another dist; Fmt

--- lib/App/PDFUtils.pm
+++ lib/App/PDFUtils.pm
@@ -26,18 +26,6 @@ my %argspec0_files = (
     },
 );
 
-my %argspec0_files__epub = (
-    files => {
-        schema => ['array*', of=>'filename*', min_len=>1,
-                   #uniq=>1, # not yet implemented by Data::Sah
-               ],
-        req => 1,
-        pos => 0,
-        slurpy => 1,
-        'x.element_completion' => [filename => {filter => sub { /\.epub$/i }}],
-    },
-);
-
 our %argspec0_file = (
     file => {
         summary => 'Input file',
@@ -74,25 +62,25 @@ our %argspecopt_return_output_file = (
     return_output_file => {
         summary => 'Return the path of output file instead',
         schema => 'bool*',
-        description => <<'_',
+        description => <<'MARKDOWN',
 
 This is useful when you do not specify an output file but do not want to show
 the converted document to stdout, but instead want to get the path to a
 temporary output file.
 
-_
+MARKDOWN
     },
 );
 
 $SPEC{add_pdf_password} = {
     v => 1.1,
     summary => 'Password-protect PDF files',
-    description => <<'_',
+    description => <<'MARKDOWN',
 
 This program is a wrapper for <prog:qpdf> to password-protect PDF files
 (in-place). This is the counterpart for <prog:remove-pdf-password>.
 
-_
+MARKDOWN
     args => {
         %argspec0_files,
         password => {
@@ -170,7 +158,7 @@ sub add_pdf_password {
 $SPEC{remove_pdf_password} = {
     v => 1.1,
     summary => 'Remove password from PDF files',
-    description => <<'_',
+    description => <<'MARKDOWN',
 
 This program is a wrapper for <prog:qpdf> to remove passwords from PDF files
 (in-place).
@@ -194,7 +182,7 @@ or:
 
     passwords = ["pass1", "pass2", "pass3"]
 
-_
+MARKDOWN
     args => {
         %argspec0_files,
         passwords => {
@@ -313,13 +301,13 @@ sub pdf_has_password {
 $SPEC{convert_pdf_to_text} = {
     v => 1.1,
     summary => 'Convert PDF file to text',
-    description => <<'_',
+    description => <<'MARKDOWN',
 
 This utility uses one of the following backends:
 
 * pdftotext
 
-_
+MARKDOWN
     args => {
         %argspec0_file,
         %argspecopt1_output,
@@ -428,7 +416,7 @@ sub convert_pdf_to_text {
 $SPEC{compress_pdf} = {
     v => 1.1,
     summary => 'Make PDF smaller',
-    description => <<'_',
+    description => <<'MARKDOWN',
 
 This utility is a wrapper for <prog:gs> (GhostScript) and is equivalent to the
 following command:
@@ -438,7 +426,7 @@ following command:
 with support for multiple files and output files automatically named
 `INPUT.compressed.pdf`.
 
-_
+MARKDOWN
     args => {
         %argspec0_files,
         %argspecopt_overwrite,
@@ -520,74 +508,13 @@ sub compress_pdf {
     $envres->as_struct;
 }
 
-$SPEC{convert_epub_to_pdf} = {
-    v => 1.1,
-    summary => 'Convert epub file to PDF',
-    description => <<'_',
-
-This utility is a simple wrapper to `ebook-convert`. It allows setting output
-filenames (`foo.epub.pdf`) so you don't have to specify them manually. It also
-allows processing multiple files in a single invocation
-
-_
-    args => {
-        %argspec0_files__epub,
-        %argspecopt_overwrite,
-    },
-    deps => {
-        prog => 'ebook-convert',
-    },
-};
-sub convert_epub_to_pdf {
-    my %args = @_;
-
-    require IPC::System::Options;
-
-    my $envres = envresmulti();
-
-    my $i = 0;
-    for my $input_file (@{ $args{files} }) {
-        log_info "[%d/%d] Processing file %s ...", ++$i, scalar(@{ $args{files} }), $input_file;
-        $input_file =~ /(.+)\.(\w+)\z/ or do {
-            $envres->add_result(412, "Please supply input file with extension in its name (e.g. foo.epub instead of foo)", {item_id=>$input_file});
-            next;
-        };
-        my ($name, $ext) = ($1, $2);
-        $ext =~ /\Aepub\z/i or do {
-            $envres->add_result(412, "Input file '$input_file' does not have .epub extension", {item_id=>$input_file});
-            next;
-        };
-
-        my $output_file = "$input_file.pdf";
-
-        if (-e $output_file) {
-            if ($args{overwrite}) {
-                log_info "Unlinking existing PDF file %s ...", $output_file;
-                unlink $output_file;
-            } else {
-                $envres->add_result(412, "Output file '$output_file' already exists, not overwriting (use --overwrite (-O) to overwrite)", {item_id=>$input_file});
-                next;
-            }
-        }
-
-        IPC::System::Options::system(
-            {log=>1},
-            "ebook-convert", $input_file, $output_file);
-        my $exit_code = $? < 0 ? $? : $? >> 8;
-        if ($exit_code) {
-            $envres->add_result(500, "ebook-convert didn't return successfully, exit code=$exit_code", {item_id=>$input_file});
-        } else {
-            $envres->add_result(200, "OK", {item_id=>$input_file});
-        }
-    } # for $input_file
-
-    $envres->as_struct;
-}
-
 1;
 # ABSTRACT: Command-line utilities related to PDF files
 
 =head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
 
 This distribution provides tha following command-line utilities related to PDF
 files:
